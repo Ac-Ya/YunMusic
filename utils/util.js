eecc328734337468fe54256c988374fae4f03aa2
Ac-Ya <2010262955@qq.com>
2021-08-04 14:45:54 +0800

    添加时间戳转化日期函数,数字格式化功能函数

--- utils/util.js
+++ utils/util.js
@@ -9,40 +9,68 @@ const formatTime = date => {
   return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
 }
 
+// 时间戳格式化
+const timestampFormat= num =>{
+  let time = new Date(num)
+  let day = time.getDate()
+  let month =time.getMonth()+1
+  return `${month}月${formatNumber(day)}日`
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : `0${n}`
 }
 
+//格式化音乐时间
+const formatMusicTime = (time) => {
+   let totalSecond = Math.ceil(time/1000)
+   let minute = parseInt(totalSecond / 60)
+   let second = totalSecond - minute*60
+   
+   return `${formatNumber(minute)}:${formatNumber(second)}`
+   
+}
+
+
 //防抖函数 在一定时间内不管点击多少次只有最后一次执行
-function debounce(fun,delay = 2000){
+const debounce = (fun,delay = 500)=>{
   let timer = null 
   return function(){
     let args = arguments
     let firstClick = !timer
     if(firstClick){
-      fun.apply(this,args)
+       fun.apply(this,args)
     }
     if(timer){
       clearInterval(timer)
       timer = null
     }
     timer = setTimeout(()=>{
-      fun.apply(this,args)
+       fun.apply(this,args)
     },delay)
   }
 }
 
-function buttonClicked(self) {
- self.setData({
-  buttonClicked: true
- })
- setTimeout(function () {
-  self.setData({
-   buttonClicked: false
-  })
- }, 1000)
-}
+     // num接收的数字,point保留数字的第几位
+ const tranNumber = (num, point) => {
+      // 将数字转换为字符串,然后通过split方法用.分隔,取到第0个
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 5) { // 判断数字有多长,如果小于5,表示1万以内的数字,让其直接显示
+          console.log(numStr);
+          return numStr;
+      } else if (numStr.length >= 5 && numStr.length <= 8) { // 如果数字大于5位,小于8位,让其数字后面加单位万
+          let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point)
+          console.log(decimal);
+          // 由千位,百位组成的一个数字
+          return parseFloat(parseInt(num / 10000) + '.' + decimal) + '万'
+      } else if (numStr.length > 8) { // 如果数字大于8位,让其数字后面加单位亿
+          let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point);
+          console.log(decimal);
+          return parseFloat(parseInt(num / 100000000) + '.' + decimal) + '亿'
+      }
+  }
+
 
 
 
@@ -50,5 +78,7 @@ module.exports = {
   formatTime,
   formatNumber,
   debounce,
-  buttonClicked
+  formatMusicTime,
+  timestampFormat,
+  tranNumber
 }
